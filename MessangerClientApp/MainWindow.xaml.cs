5006bab6a372026afc5fd70d4d5da01d79928733
home <home@DESKTOP-4GIHFVI>
2024-03-22 15:41:49 +0200

    Add project files.

---
+++ MessangerClientApp/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessangerClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverPoint;
+        //const string serverAddress = "127.0.0.1";
+        //const short serverPort = 4040;
+        UdpClient client ;
+        ObservableCollection<MessageInfo> messages ;   
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new UdpClient();
+            string address = ConfigurationManager.AppSettings["ServerAddress"]!;
+            short port = short.Parse(ConfigurationManager.AppSettings["ServerPort"]!);
+            serverPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            messages = new ObservableCollection<MessageInfo>();
+            this.DataContext = messages;
+            
+        }
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {           
+            string message = msgTextBox.Text;
+            SendMessage(message);
+        }
+        private async void JoinButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "$<join>";
+            SendMessage(message);
+            Listen();
+        }
+        private async void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(data, serverPoint);
+        }
+        private async void Listen()
+        {
+            while (true)
+            {
+                //IPEndPoint remoreIpServer = null;
+                var result = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                messages.Add(new MessageInfo(message));
+            }
+           
+        }
+    }
+    public class MessageInfo
+    {
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+
+        public MessageInfo(string text)
+        {
+            this.Text = text;   
+            Time = DateTime.Now;                
+        }
+        public override string ToString()
+        {
+            return $"{Text}  {Time.ToShortDateString()}";
+        }
+
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    public class ChatServer
+    {
+        const short port = 4040;
+        const string JOIN_CMD = "$<join>";
+        List<IPEndPoint> members ;
+        UdpClient server ;
+        IPEndPoint clientEndPoint = null;
+        public ChatServer()
+        {
+            members = new List<IPEndPoint>();
+            server = new UdpClient(port);
+        }
+        private void AddMember(IPEndPoint clientEndPoint)
+        {
+            members.Add(clientEndPoint);
+            Console.WriteLine("Member was added !");
+        }
+        private void SendToAll(byte[] data)
+        {
+            foreach (var member in members)
+            {
+                server.SendAsync(data, data.Length, member);
+            }
+        }
+        public void Start()
+        {           
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine($" Got message {message}  from : {clientEndPoint} " +
+                    $"at {DateTime.Now.ToShortTimeString()}");
+                switch (message)
+                {
+                    case JOIN_CMD:
+                        AddMember(clientEndPoint);
+                        break;
+                    default:
+                        SendToAll(data);
+                        break;
+                }
+            }
+
+        }
+    }
+    internal class Program
+    {       
+        static void Main(string[] args)
+        {
+            ChatServer server = new ChatServer();   
+            server.Start();
+              
+            
+        }
+       
+    }
+}
